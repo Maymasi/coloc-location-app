6eef57e403d5a1c56926f06aaa3c44e245784219
oussama <oussamanouhar0@gmail.com>
2025-04-12 04:55:13 +0100

    feat: add left and top navigation

---
+++ src/components/common/LeftNav.jsx
@@ -0,0 +1,106 @@
+import { X,House,Heart,MessageSquare,Users,SquareLibrary,Bell,Settings,User} from 'lucide-react';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
+
+import '../../assets/styles/LeftNav.css';
+export default  function LeftNav(){
+    const [activeLink, setActiveLink] = React.useState(null);
+
+    const handleLinkClick = (linkName) => {
+        setActiveLink(linkName);
+    };
+
+    return(
+            <div className="left-nav">
+                <div className="logo">
+                     <img src="/src/assets/logos/Logo.png" alt="Logo" />
+                    <X />
+                </div>
+                <Divider style={{backgroundColor:'hsl(0 ,0%, 92%)'}}/>
+                
+                    <div className="content-nav">
+                        <div className="user">
+                            <Avatar  sx={{ width: 38, height: 38 }} style={{backgroundColor:'black'}} alt="user" src="/src/assets/logos/logoColokMeak.png" />
+                            <div className="user-data">
+                                <div className='name'>User Name</div>
+                                <div className='role'>Student</div>
+                            </div>
+                        </div>
+                        <div className="nav-links">                           
+                                <div 
+                                    className={`link ${activeLink === 'dashboard' ? 'active' : ''}`} 
+                                    onClick={() => handleLinkClick('dashboard')}
+                                >
+                                    <House size={20} />
+                                    <span>Tableau de Bord</span>
+                                </div>
+                                <div 
+                                    className={`link ${activeLink === 'annonces' ? 'active' : ''}`} 
+                                    onClick={() => handleLinkClick('annonces')}
+                                >
+                                    <House size={20} />
+                                    <span>Annonces</span>
+                                </div>
+                                <div 
+                                    className={`link ${activeLink === 'favoris' ? 'active' : ''}`} 
+                                    onClick={() => handleLinkClick('favoris')}
+                                >
+                                    <Heart size={20} />
+                                    <span>Favoris</span>
+                                </div>
+                                <div 
+                                    className={`link ${activeLink === 'messages' ? 'active' : ''}`} 
+                                    onClick={() => handleLinkClick('messages')}
+                                    style={{justifyContent:'space-between'}}
+                                >
+                                    <div className="" style={{display:'flex',alignItems:'center',gap:'1rem'}}>
+                                        <MessageSquare size={20} />
+                                        <span>Messages</span>
+                                    </div>
+                                    <span className="notification">3</span>
+                                </div>
+                                <div 
+                                    className={`link ${activeLink === 'colocations' ? 'active' : ''}`} 
+                                    onClick={() => handleLinkClick('colocations')}
+                                >
+                                    <Users size={20} />
+                                    <span>Colocations</span>
+                                </div>
+                                <div 
+                                    className={`link ${activeLink === 'demandesRecus' ? 'active' : ''}`} 
+                                    onClick={() => handleLinkClick('demandesRecus')}
+                                    style={{justifyContent:'space-between'}}
+                                >
+                                    <div className="" style={{display:'flex',alignItems:'center',gap:'1rem'}}>
+                                        <Bell size={20} />
+                                        <span>Demandes recus</span>
+                                    </div>
+                                    <span className="notification">5</span>
+                                </div>
+                                <div 
+                                    className={`link ${activeLink === 'mesDemandes' ? 'active' : ''}`} 
+                                    onClick={() => handleLinkClick('mesDemandes')}
+                                >
+                                    <SquareLibrary size={20} />
+                                    <span>Mes demandes</span>
+                                </div>
+                                <div 
+                                    className={`link ${activeLink === 'profil' ? 'active' : ''}`} 
+                                    onClick={() => handleLinkClick('profil')}
+                                >
+                                    <User size={20} />
+                                    <span>Profil</span>
+                                </div>
+                                <div 
+                                    className={`link ${activeLink === 'parametres' ? 'active' : ''}`} 
+                                    onClick={() => handleLinkClick('parametres')}
+                                >
+                                     <Settings size={20} />
+                                    <span>Parametres</span>
+                                </div>
+                        </div>
+                    </div>
+            </div>
+    )
+}
---
+++ src/components/common/TopNav.jsx
@@ -0,0 +1,208 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import { User, BellDot, MessageCircle } from 'lucide-react';
+import '../../assets/styles/TopNav.css';
+
+export default function TopNav() {
+    const [userAnchorEl, setUserAnchorEl] = React.useState(null);
+    const [notifAnchorEl, setNotifAnchorEl] = React.useState(null);
+
+    const userMenuOpen = Boolean(userAnchorEl);
+    const notifMenuOpen = Boolean(notifAnchorEl);
+
+    const handleUserMenuClick = (event) => {
+        setUserAnchorEl(event.currentTarget);
+    };
+
+    const handleNotifMenuClick = (event) => {
+        setNotifAnchorEl(event.currentTarget);
+    };
+
+    const handleUserMenuClose = () => {
+        setUserAnchorEl(null);
+    };
+
+    const handleNotifMenuClose = () => {
+        setNotifAnchorEl(null);
+    };
+
+    return (
+        <AppBar
+            position="fixed"
+            style={{ alignItems: 'end' }}
+            sx={{
+                justifyContent: 'center',
+                zIndex: 1201,
+                backgroundColor: "white",
+                height: '80px',
+                boxShadow: 'none',
+                borderBottom: '1px solid hsl(0, 0%, 92%)',
+                width: '100%',
+                padding: '0 1rem',
+                margin: '0 auto',
+                alignItems: 'center'
+            }}
+        >
+            <Toolbar style={{ gap: '1rem' }}>
+                <React.Fragment>
+                    {/* Notification Icon */}
+                    <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                        <BellDot
+                            size={10}
+                            style={{ color: 'black' }}
+                            onClick={handleNotifMenuClick}
+                            aria-controls={notifMenuOpen ? 'notif-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={notifMenuOpen ? 'true' : undefined}
+                            className='user-icon'
+                        />
+                    </Box>
+
+                    {/* User Icon */}
+                    <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                        <User
+                            size={10}
+                            style={{ color: 'black' }}
+                            onClick={handleUserMenuClick}
+                            aria-controls={userMenuOpen ? 'account-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={userMenuOpen ? 'true' : undefined}
+                            className='user-icon'
+                        />
+                    </Box>
+
+                    {/* User Menu */}
+                    <Menu
+                        style={{ borderRadius: '12px' }}
+                        anchorEl={userAnchorEl}
+                        id="account-menu"
+                        open={userMenuOpen}
+                        onClose={handleUserMenuClose}
+                        onClick={handleUserMenuClose}
+                        slotProps={{
+                            paper: {
+                                elevation: 0,
+                                sx: {
+                                    borderRadius: '12px',
+                                    overflow: 'visible',
+                                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                    mt: 1.5,
+                                    '& .MuiAvatar-root': {
+                                        width: 32,
+                                        height: 32,
+                                        ml: -0.5,
+                                        mr: 1,
+                                    },
+                                    '&::before': {
+                                        content: '""',
+                                        display: 'block',
+                                        position: 'absolute',
+                                        top: 0,
+                                        right: 14,
+                                        width: 10,
+                                        height: 10,
+                                        bgcolor: 'background.paper',
+                                        transform: 'translateY(-50%) rotate(45deg)',
+                                        zIndex: 0,
+                                    },
+                                },
+                            },
+                        }}
+                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                    >
+                        <MenuItem onClick={handleUserMenuClose}>
+                            <Avatar /> Mon Compte
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem onClick={handleUserMenuClose}>
+                            <ListItemIcon>
+                                <PersonAdd fontSize="small" />
+                            </ListItemIcon>
+                            Profil
+                        </MenuItem>
+                        <MenuItem onClick={handleUserMenuClose}>
+                            <ListItemIcon>
+                                <Settings fontSize="small" />
+                            </ListItemIcon>
+                            Paramètres
+                        </MenuItem>
+                        <MenuItem onClick={handleUserMenuClose}>
+                            <ListItemIcon>
+                                <Logout fontSize="small" />
+                            </ListItemIcon>
+                            Déconnexion
+                        </MenuItem>
+                    </Menu>
+
+                    {/* Notification Menu */}
+                    <Menu
+                        sx={{ borderRadius: '12px' }}
+                        anchorEl={notifAnchorEl}
+                        id="notif-menu"
+                        open={notifMenuOpen}
+                        onClose={handleNotifMenuClose}
+                        onClick={handleNotifMenuClose}
+                        slotProps={{
+                            paper: {
+                                elevation: 0,
+                                sx: {
+                                    borderRadius: '12px',
+                                    overflow: 'visible',
+                                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                    mt: 1.5,
+                                    '& .MuiAvatar-root': {
+                                        width: 32,
+                                        height: 32,
+                                        ml: -0.5,
+                                        mr: 1,
+                                    },
+                                    '&::before': {
+                                        content: '""',
+                                        display: 'block',
+                                        position: 'absolute',
+                                        top: 0,
+                                        right: 14,
+                                        width: 10,
+                                        height: 10,
+                                        bgcolor: 'background.paper',
+                                        transform: 'translateY(-50%) rotate(45deg)',
+                                        zIndex: 0,
+                                    },
+                                },
+                            },
+                        }}
+                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                    >
+                        <MenuItem onClick={handleNotifMenuClose}>
+                            Notifications
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem onClick={handleNotifMenuClose}>
+                            Nouveau message
+                        </MenuItem>
+                        <MenuItem onClick={handleNotifMenuClose}>
+                            Demande de colocation
+                        </MenuItem>
+                    </Menu>
+                </React.Fragment>
+            </Toolbar>
+        </AppBar>
+    );
+}
